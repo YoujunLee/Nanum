b6971782e9f398126b22fd72c6799ab758961d7c
Youjun Lee <injxyj@gmail.com>
2015-01-23 11:03:39 +0000

    carrierwave gem 사용 준비하기

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ActiveRecord::Base
 	validates :category, :inclusion => { :in => ["중고장터", "공동구매", "나눔", "카풀"], :message => "카테고리를 하나 선택하셔야 합니다."}
 	validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다."}
 	validates :content, :presence => { :message => "내용을 반드시 입력하셔야 합니다."}
+	mount_uploader :image, ImageUploader
 end
